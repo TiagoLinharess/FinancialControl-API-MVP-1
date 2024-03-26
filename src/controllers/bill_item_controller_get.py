00e380098294d1e2e9a472ffb7263d71d5189c1d
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-03-26 16:52:19 -0300

    Implements get route

--- src/controllers/bill_item_controller_get.py
+++ src/controllers/bill_item_controller_get.py
@@ -1,15 +1,55 @@
 from flask import Blueprint, request
 from repositories.year import YearRepository
 from repositories.month import MonthRepository
-from models import Session, Year
-from schemas.default import DefaultRequestSchema
+from models import Session, Year, Month
+from schemas.default import DefaultResponseSchema, get_default_list
+from typing import List
 
 get_bill_items = Blueprint("get_bill_items", __name__)
 
-# Rota de POST do endpoint de Bill Items
+# Rota de GET do endpoint de Bill Items
 @get_bill_items.get('/bill_items')
 def read_bill_item():
-    return {
-        "name": "Conta Luz - Janeiro - 2024",
-        "value": 400
-    }, 200
+    try:
+        # Cria sessão
+        session = Session()
+
+        # Procura anos
+        years = get_years(session)
+
+        # Loop para buscar os meses
+        for year in years:
+
+            # Procura os meses
+            months = get_months(session, year)
+
+            # Atribui meses ao ano
+            year.months = months
+
+        return get_default_list(years)
+    except Exception as e:
+        # Retorno de erro da rota
+        return { "error": str(e) }, 400
+
+
+def get_years(session: Session) -> List[Year]:
+    try:
+        # Instancia repositório
+        year_repository = YearRepository(session)
+
+        # Busca anos se existirem no repositório
+        return year_repository.read()
+    except Exception as e:
+        # Tratativa de erro do repostirório
+        raise ValueError("Error on fetching year: " + str(e))
+
+def get_months(session: Session, year: Year) -> List[Month]:
+    try:
+        # Instancia repositório
+        month_repository = MonthRepository(session)
+
+        # Busca meses se existirem no repositório
+        return month_repository.read(year)
+    except Exception as e:
+        # Tratativa de erro do repostirório
+        raise ValueError("Error on fetching month: " + str(e))
--- src/schemas/default.py
+++ src/schemas/default.py
@@ -1,4 +1,8 @@
-class DefaultRequestSchema():
+import json
+from models import Year, Month
+from typing import List
+
+class DefaultRequestSchema:
 
     # Inicializa request
     def __init__(self, year: str, month: str):
@@ -12,3 +16,48 @@ class DefaultRequestSchema():
     # Busca valor de MONTH
     def get_month(self) -> str:
         return self.__month
+
+class DefaultResponseSchema:
+
+    # Inicializa response
+    def __init__(self, year: Year):
+        self.year: str = year.year
+        self.months: List[MonthSchema] = list(map(get_month, year.months))
+    
+    # Transforma objeto em json
+    def to_json(self):
+        return {
+            "year": self.year,
+            "months": list(map(get_month_json, self.months))
+        }
+
+class MonthSchema:
+
+    # Inicializa response
+    def __init__(self, month: Month):
+        self.month: str = month.month
+    
+    # Transforma objeto em json
+    def to_json(self):
+        return {
+            "month": self.month,
+            "incomes": [],
+            "outcomes": []
+        }
+    
+
+# Transforma model Month para MonthSchema
+def get_month(month: Month) -> MonthSchema:
+    return MonthSchema(month)
+
+# Transforma model MonthSchema para Json
+def get_month_json(month: Month):
+    return MonthSchema(month).to_json()
+
+# Transforma model DefaultResponseSchema para Json
+def get_schema_json(year: Year):
+    return DefaultResponseSchema(year).to_json()
+
+# Transforma lista Years para Json
+def get_default_list(years: List[Year]):
+    return list(map(get_schema_json, years))
