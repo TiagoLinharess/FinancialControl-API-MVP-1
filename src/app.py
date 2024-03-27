8df35f5ec5e152cb5511508170ab486e2478ec78
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-03-27 15:21:17 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,11 +7,3 @@ app = Flask(__name__)
 
 # Inicializa rotas
 app.register_blueprint(api)
-
-# @app.post('/bill_items/<int:id>')
-# def update_item(id: int):
-#     return { "success": True, "id": id }, 201
-
-# @app.delete('/bill_items/<int:id>')
-# def delete_item(id: int):
-#     return { "success": True, "id": id }, 201
