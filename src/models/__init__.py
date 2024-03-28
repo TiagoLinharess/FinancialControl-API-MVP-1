bfd2e299c541312d252ea337b4c9d3dc6b9aabee
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-03-28 10:39:56 -0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,21 @@
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
 
 from models.base import Base
 from models.item import Item
 from models.month import Month
 from models.year import Year
 
+db_path = "database/"
+# Verifica se o diretorio não existe
+if not os.path.exists(db_path):
+   # então cria o diretorio
+   os.makedirs(db_path)
+
 # Link do banco de dados
-db_url = 'sqlite:///database/db.sqlite3'
+db_url = 'sqlite:///%s/db.sqlite3' % db_path
 
 # Cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
